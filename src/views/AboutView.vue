12f3d5b62f762c6257e0cb85184e5d42d0da755e
Patara.m <patara.m@cartrack.com>
2022-08-14 16:53:47 +0700

    add axios and test call api map-info

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an test about page</h1>\
+    <div>
+      {{responseData}}
+    </div>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+import { config } from '../../config';
+
+export default {
+  name: "Rover-page",
+  data() {
+    return {
+      responseData:null,
+      apiUrl: config.$api_url
+    }
+  },
+  mounted () {
+    axios
+        .get(this.apiUrl+'/map-info')
+        .then(response => (this.responseData = response.data))
+    }
+  }
+
+</script>
